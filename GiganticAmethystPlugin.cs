ea2fe90f2742007abd97fd7c22b0190532928f96
some dude <kurogames7595@gmail.com>
2020-04-16 03:45:20 -0400

    https://github.com/RicoValdezio/GigaanticAmethyst/issues/1

--- GiganticAmethystPlugin.cs
+++ GiganticAmethystPlugin.cs
@@ -1,4 +1,5 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using R2API;
 using R2API.Utils;
 using RoR2;
@@ -7,19 +8,71 @@ using UnityEngine;
 
 namespace GiganticAmethyst
 {
-    [BepInDependency("com.bepis.r2api")]
-    [R2APISubmoduleDependency(nameof(ItemAPI), nameof(ItemDropAPI), nameof(ResourcesAPI))]
-    [BepInPlugin(ModGuid, ModName, ModVer)]
-    public class WickedRing : BaseUnityPlugin
+    [BepInDependency("com.bepis.r2api",  BepInDependency.DependencyFlags.HardDependency)]
+    //Sushi/Engima - Use an array to request submodules
+    [R2APISubmoduleDependency(new string[]
     {
-        private const string ModVer = "1.0.0";
+        "ItemAPI",
+        "ItemDropAPI",
+        "ResourcesAPI",
+    })]
+    [BepInPlugin(ModGuid, ModName, ModVer)]
+    //Sushi/Engima - Was this a wickedring mod?
+    //Sushi/Engima - You can hook the itemcatalog, check if the itemdef is the wickedring, then make your changes if you're still interested in making one. 
+    //Sushi/Engima - public class WickedRing : BaseUnityPlugin
+    public class GiganticAmethyst : BaseUnityPlugin
+    {
+        private const string ModVer = "1.1.0";
         private const string ModName = "GiganticAmethyst";
         private const string ModGuid = "com.RicoValdezio.GiganticAmethyst";
 
+        //Sushi/Engima - Make it static so it's accessible
+        public static ConfigEntry<bool> RoR1Behavior;
+        public static ConfigEntry<float> Cooldown;
         private void Awake()
         {
+            //Sushi/Engima - Set our values before doing anything else
+            InitConfig();
+
             GiganticAmethystEquip.Init();
-            GiganticAmethystHook.Init();
+
+            //Sushi/Engima - Subscribe instead of making a whole new class. 
+            On.RoR2.EquipmentSlot.PerformEquipmentAction += EquipmentSlot_PerformEquipmentAction;
+        }
+        private bool EquipmentSlot_PerformEquipmentAction(On.RoR2.EquipmentSlot.orig_PerformEquipmentAction orig, EquipmentSlot self, EquipmentIndex equipmentIndex)
+        {
+            if (equipmentIndex == GiganticAmethystEquip.AmethystEquipmentIndex)
+            {
+                SkillLocator skillLocator = self.characterBody.skillLocator;
+                if (skillLocator)
+                {
+                    //Sushi/Engima - There's one aspect of mod development that you should know of
+                    //Sushi/Engima - Praying to god that the mod doesn't need additional networking
+                    if (RoR1Behavior.Value) skillLocator.ResetSkills();
+                    //Sushi/Engima - It's an on and off behavior, if it isn't on it's off and vice versa.
+                    else skillLocator.ApplyAmmoPack();
+                    //Sushi/Engima - Watch it completely error though!
+                }
+                return true;
+            }
+            //Sushi/Engima - Gotta do this.
+            return orig(self, equipmentIndex);
+        }
+        private void InitConfig()
+        {
+            //Sushi/Enigma - y'know, this reminds me of a github pull request
+            //https://github.com/space-wizards/space-station-14/pull/801
+            RoR1Behavior = Config.Bind<bool>(
+            "RoR1Behavior",
+            "Uses the RoR1 behavior. Turn this off to use an alternate behavior.",
+            true
+            );
+            //Sushi/Enigma - Made a cooldown option because let's be completely honest here, no one wants to dive into the code every time they want to change a fucking int.
+            Cooldown = Config.Bind<float>(
+            "Cooldown",
+            "The cooldown of the equipment. Is a float.", 
+            8
+            );
         }
     }
 
@@ -30,6 +83,7 @@ namespace GiganticAmethyst
         internal static AssetBundleResourcesProvider AmethystProvider;
         internal static AssetBundle AmethystBundle;
 
+        //Sushi/Engima - I haven't done assetbundle stuff at all.
         private const string ModPrefix = "@GiganticAmethyst:";
         private const string PrefabPath = ModPrefix + "Assets/Amethyst.prefab";
         private const string IconPath = ModPrefix + "Assets/Amethyst_Icon.png";
@@ -49,20 +103,29 @@ namespace GiganticAmethyst
 
         private static void AmethystAsEquip()
         {
+            //Sushi/Engima - Do our token stuff here.
+            R2API.AssetPlus.Languages.AddToken("AMETHYST_NAME", "Gigantic Amethyst");
+            R2API.AssetPlus.Languages.AddToken("AMETHYST_PICKUP", "<style=cIsUtility>Reset</style> all your cooldowns.");
+            R2API.AssetPlus.Languages.AddToken("AMETHYST_DESCRIPTION", "<style=cIsUtility>Reset</style> all your cooldowns on activation.");
+            //Sushi/Engima - I changed the lore.
+            R2API.AssetPlus.Languages.AddToken("AMETHYST_LORE", "I highly suggest handling this thing with some form of protective gear; we're not sure if it has any effects on the human body.");
             EquipmentDef AmethystEquipmentDef = new EquipmentDef
-            {
-                name = "GiganticAmethystEquipment",
-                cooldown = 30f,
+            {   
+                name = "AMETHYST_NAME",
+                //Sushi/Engima - Doubled the cooldown
+                cooldown = GiganticAmethyst.Cooldown.Value,
                 pickupModelPath = PrefabPath,
                 pickupIconPath = IconPath,
-                nameToken = "Gigantic Amethyst",
-                pickupToken = "<style=cIsUtility>Reset</style> all your cooldowns.",
-                descriptionToken = "<style=cIsUtility>Reset</style> all your cooldowns.",
-                loreToken = "Used for focus lasers, so they say.",
+                //Sushi/Engima - Changed these fields so they correctly used tokens
+                nameToken = "AMETHYST_NAME",
+                pickupToken = "AMETHYST_PICKUP",
+                descriptionToken = "AMETHYST_DESCRIPTION",
+                loreToken = "AMETHYST_LORE",
                 canDrop = true,
                 enigmaCompatible = true
             };
-            
+
+            //Sushi/Engima - idk what this is lol
             ItemDisplayRule[] AmethystDisplayRules = new ItemDisplayRule[1];
             AmethystDisplayRules[0].followerPrefab = AmethystPrefab;
             AmethystDisplayRules[0].childName = "Chest";
@@ -73,39 +136,6 @@ namespace GiganticAmethyst
             CustomEquipment AmethystEquipment = new CustomEquipment(AmethystEquipmentDef, AmethystDisplayRules);
 
             AmethystEquipmentIndex = ItemAPI.Add(AmethystEquipment);
-        }
-    }
-
-    internal class GiganticAmethystHook
-    {
-        internal static void Init()
-        {
-            On.RoR2.EquipmentSlot.PerformEquipmentAction += (orig, self, equipmentIndex) =>
-            {
-                if (equipmentIndex == GiganticAmethystEquip.AmethystEquipmentIndex)
-                {
-                    SkillLocator Skills = self.characterBody.skillLocator;
-                    if (Skills.primary)
-                    {
-                        Skills.primary.Reset();
-                    }
-                    if (Skills.secondary)
-                    {
-                        Skills.secondary.Reset();
-                    }
-                    if (Skills.utility)
-                    {
-                        Skills.utility.Reset();
-                    }
-                    if (Skills.special)
-                    {
-                        Skills.special.Reset();
-                    }
-                    return true;
-                }
-
-                return orig(self, equipmentIndex);
-            };
         }
     }
 }
